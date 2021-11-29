0f5c96e583a6cb70b142d20ab6caa5e3e33faf41
wpo9nine <wpo9nine@gmail.com>
2021-11-30 00:06:11 +0700

    add laporan pemesanan

--- src/cview.py
+++ src/cview.py
@@ -120,6 +120,12 @@ class userDashboard(QDialog):
         loadUi("dashboard.ui", self)
         self.logout.clicked.connect(self.gotowelscreen)
         self.paket.clicked.connect(self.choosepaket)
+        self.report.clicked.connect(self.movetoorderreport)
+
+    def movetoorderreport(self):
+        report = laporanPemesanan()
+        widget.addWidget(report)
+        widget.setCurrentIndex(widget.currentIndex()+1)
 
     def choosepaket(self):
         paket = pilihPaket()
@@ -405,7 +411,7 @@ class modulKonfirmasiPembayaran(QDialog):
 
 
 class MainWindow(QDialog):
-
+    upload = False
     def __init__(self):
         super(MainWindow, self).__init__()
         loadUi("gui.ui", self)
@@ -414,7 +420,7 @@ class MainWindow(QDialog):
         self.downloadfile.clicked.connect(self.gotoPostFile)
         self.upload.clicked.connect(self.uploadfile)
         self.deletefile.clicked.connect(self.delfile)
-        self.gohome.clicked.connect(self.movetoDashboard)
+        
 
     def browsefiles(self):
         global fname, _
@@ -454,6 +460,10 @@ class MainWindow(QDialog):
         conn.commit()
         conn.close()
         self.notif.setText("Upload Success!")
+        self.upload = True
+        if self.upload:
+            self.gohome.clicked.connect(self.movetoDashboard)
+            self.upload = False
 
     def delfile(self):
         conn = sqlite3.connect("database.db")
@@ -469,10 +479,17 @@ class MainWindow(QDialog):
         conn.commit()
         conn.close()
         self.notif.setText("Delete Success!")
+        if self.upload == False:
+            self.gohome.clicked.connect(self.movetoself)
    
     def movetoDashboard(self):
         dashboard = userDashboard()
         widget.addWidget(dashboard)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+    
+    def movetoself(self):
+        mainwd = MainWindow()
+        widget.addWidget(mainwd)
         widget.setCurrentIndex(widget.currentIndex()+1)
 
 class PostFile(QDialog):
@@ -484,6 +501,142 @@ class PostFile(QDialog):
     def gotoMainWindow(self):
         mainwindow = MainWindow()
         widget.addWidget(mainwindow)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+class laporanPemesanan(QDialog):
+    def __init__(self):
+        super(laporanPemesanan, self).__init__()
+        loadUi("laporanPemesanan.ui", self)
+        self.Home.clicked.connect(self.movetoPagePembayaran)
+        self.detailPaket.clicked.connect(self.gotopaket)
+        self.detailBayar.clicked.connect(self.gotobayar)
+        self.detailCV.clicked.connect(self.gotocv)
+        self.tabelPemesanan.setColumnWidth(0, 50)
+        self.tabelPemesanan.setColumnWidth(1, 100)
+        # self.loadData()
+
+    def movetoPagePembayaran(self):
+        dashboard = userDashboard()
+        widget.addWidget(dashboard)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+    def gotopaket(self):
+        paket = detailPaket()
+        widget.addWidget(paket)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+    def gotobayar(self):
+        bayar = detailBayar()
+        widget.addWidget(bayar)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+    def gotocv(self):  
+        cv = detailCV()
+        widget.addWidget(cv)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+    # def loadData(self):
+    #     conn = sqlite3.connect("CView.db")
+    #     cur = conn.cursor()
+    #     query = "SELECT * FROM 'Order' LIMIT 5"
+
+    #     tableRow = 0
+    #     for row in cur.execute(query):
+    #         self.tabelPemesanan.setItem(tableRow, 1, QtWidgets.QTablelWidgetItem(row[0]))
+    #         self.tabelPemesanan.setItem(tableRow, 2, QtWidgets.QTablelWidgetItem(row[1]))
+    #         tableRow += 1
+
+class detailPaket(QDialog):
+    def __init__(self):
+        super(detailPaket, self).__init__()
+        loadUi("detailPaket.ui", self)
+        self.Back.clicked.connect(self.gotoback)
+        conn = sqlite3.connect("CView.db")
+        cur = conn.cursor()
+        qdate = 'SELECT "Order"."Tanggal Pemesanan" FROM "Order", Pembayaran as P  WHERE P.ID_Order = "Order".ID'
+        cur.execute(qdate)
+        date = cur.fetchone()[0]
+        self.LTanggal.setText(date)
+
+        qpaket = 'SELECT P.ID FROM "Order", Paket as P  WHERE P.ID_Order = "Order".ID'
+        cur.execute(qpaket)
+        paket = cur.fetchone()[0]
+        self.LPaket.setText("Paket " + str(paket))
+
+        qdurasi = 'SELECT P.Durasi FROM "Order", Paket as P  WHERE P.ID_Order = "Order".ID'
+        cur.execute(qdurasi)
+        durasi = cur.fetchone()[0]
+        self.LDurasi.setText(str(durasi) + " Hari")
+
+        qCV = 'SELECT P.Jumlah_CV FROM "Order", Paket as P  WHERE P.ID_Order = "Order".ID'
+        cur.execute(qCV)
+        CV = cur.fetchone()[0]
+        self.LJumlah.setText(str(CV))
+
+        qharga = 'SELECT P.Harga FROM "Order", Paket as P  WHERE P.ID_Order = "Order".ID'
+        cur.execute(qharga)
+        harga = cur.fetchone()[0]
+        self.LHarga.setText("Rp " + str(harga))
+
+    def gotoback(self):  
+        Order = laporanPemesanan()
+        widget.addWidget(Order)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+class detailCV(QDialog):
+    def __init__(self):
+        super(detailCV, self).__init__()
+        loadUi("detailCV.ui", self)
+        self.Back.clicked.connect(self.gotoback)
+        conn = sqlite3.connect("CView.db")
+        cur = conn.cursor()
+        qdate = 'SELECT "Order"."Tanggal Pemesanan" FROM "Order", CV  WHERE CV.ID_Order = "Order".ID'
+        cur.execute(qdate)
+        date = cur.fetchone()[0]
+        self.LTanggal.setText(date)
+
+        qCV = 'SELECT CV.Jumlah_CV FROM "Order", CV  WHERE CV.ID_Order = "Order".ID'
+        cur.execute(qCV)
+        CV = cur.fetchone()[0]
+        self.LCV.setText(str(CV))
+
+        qStatus = 'SELECT CV.Status FROM "Order", CV  WHERE CV.ID_Order = "Order".ID'
+        cur.execute(qStatus)
+        Status = cur.fetchone()[0]
+        self.LStatus.setText(Status)
+
+    def gotoback(self):  
+        Order = laporanPemesanan()
+        widget.addWidget(Order)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+class detailBayar(QDialog):
+    def __init__(self):
+        super(detailBayar, self).__init__()
+        loadUi("detailBayar.ui", self)
+        self.Back.clicked.connect(self.gotoback)
+        conn = sqlite3.connect("CView.db")
+        cur = conn.cursor()
+        qdate = 'SELECT "Order"."Tanggal Pemesanan" FROM "Order", Pembayaran as P  WHERE P.ID_Order = "Order".ID'
+        cur.execute(qdate)
+        date = cur.fetchone()[0]
+        self.LTanggal.setText(date)
+
+        qmethod = 'SELECT P.Metode FROM "Order", Pembayaran as P  WHERE P.ID_Order = "Order".ID'
+        cur.execute(qmethod)
+        method = cur.fetchone()[0]
+        self.LMetode.setText(method)
+
+        qbayar = 'SELECT P.Total_Bayar FROM "Order", Pembayaran as P  WHERE P.ID_Order = "Order".ID'
+        cur.execute(qbayar)
+        bayar = cur.fetchone()[0]
+        self.LBayar.setText("Rp " + str(bayar))
+
+        qstatus = 'SELECT P.Status FROM "Order", Pembayaran as P  WHERE P.ID_Order = "Order".ID'
+        cur.execute(qstatus)
+        status = cur.fetchone()[0]
+        self.LStatus.setText(status)
+
+    def gotoback(self):  
+        Order = laporanPemesanan()
+        widget.addWidget(Order)
         widget.setCurrentIndex(widget.currentIndex()+1)
 
 
